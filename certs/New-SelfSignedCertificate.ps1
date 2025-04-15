891eb994615e2531b85d033b293d737491fd8286
Ali <64026099+Aliko47@users.noreply.github.com>
2025-04-15 09:51:08 +0200

    Add files via upload

---
+++ certs/New-SelfSignedCertificate.ps1
@@ -0,0 +1,8 @@
+﻿# Create certificate
+$mycert = New-SelfSignedCertificate -DnsName "organization.com" -CertStoreLocation "cert:\LocalMachine\My" -NotAfter (Get-Date).AddYears(5) -KeySpec KeyExchange
+
+# Export certificate to .pfx file
+#$mycert | Export-PfxCertificate -FilePath $PSScriptRoot\ERA-Report-Cert.pfx -Password (Get-Credential).password
+
+# Export certificate to .cer file
+$mycert | Export-Certificate -FilePath $PSScriptRoot\ERA-Report-Cert.cer
